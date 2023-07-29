1d4bbca8192abad922ad0c641b9b89330385dba2
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-29 16:00:33 +0500

    CODE RUB:Create and Update Data

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Address;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function address(){
+        return $this->hasOne(Address::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Models\User;
+use App\Models\Address;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert', function () {
+    $user = User::findOrFail(1);
+
+    $address = new Address(['name'=>'221B Baker Street avenue ']);
+
+    $user->address()->save($address);
+});
+
+Route::get('/update', function() {
+    $address = Address::whereUserId(1)->first();
+
+    $address->name = " 2111 Update avenue , Alaska";
+
+    $address->save();
+
 });
